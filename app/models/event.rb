74ccb608c4c6a3a90cf868e75f6c083460c72047
Sin63 <sincov63@gmail.com>
2020-12-18 17:35:33 +0300

    Added validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
-  validates :title, presence :true, length: {maximum: 255}
-  validates :address, presence :true
-  validates :datetime, presence :true
+  belongs_to :user
+  
+  validates :user, presence: true
+  validates :title, presence: true, length: {maximum: 255}
+  validates :address, presence: true
+  validates :datetime, presence: true
 end
