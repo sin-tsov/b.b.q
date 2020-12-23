e8679a113b15a0db61288606f710328b32412c31
Sin63 <sincov63@gmail.com>
2020-12-23 14:06:33 +0300

    First alpha version ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, exept: [:show]
+  before_action :set_current_user, exept: [:show]
   
   # GET /users/1
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/1/edit
@@ -19,13 +21,12 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+  def set_current_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,24 @@
 module ApplicationHelper
-def user_avatar(user)
-  asset_pack_path('media/images/user.png')    
-end
-
-def bootstrap_class_for(flash_type)
-  {
-    success: 'alert-success',
-    error: 'alert-danger',
-    alert: 'alert-warning',
-    notice: 'alert-success'
-  }.stringify_keys[flash_type.to_s] || flash_type.to_s
-end
-def bootstrap_flash(opts = {})
-  flash.each do |msg_type, message|
-    concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: 'alert') do
-      concat content_tag(:button, '×', class: 'close', data: { dismiss: 'alert' })
-      concat message
-    end)
+  def user_avatar(user)
+    asset_pack_path('media/images/user.png')    
   end
-  nil
-end
-  
 
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-success'
+     }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def bootstrap_flash(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: 'alert') do
+        concat content_tag(:button, '×', class: 'close', data: { dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
 end
