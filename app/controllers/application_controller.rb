8d00faa33339c29977ae89c3df4db709c32ec03c
Sin63 <sincov63@gmail.com>
2020-12-28 00:30:04 +0300

    bootstrap setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Настройка для работы Девайза, когда юзер правит профиль
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :current_user_can_edit?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [:password, :password_confirmation, :current_password]
+    )
   end
 
-  def current_user_can_edit?(event)
+  def current_user_can_edit?(model)
     # Если у модели есть юзер и он залогиненный, пробуем у неё взять .event
     # Если он есть, проверяем его юзера на равенство current_user.
-    user_signed_in? && event.user == current_user 
+    user_signed_in? && (
+      model.user == current_user ||
+      (model.try(:event).present? && model.event.user == current_user)
+    )
   end
 end
--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,6 +1,17 @@
 require("@rails/ujs").start()
-import 'bootstrap/dist/js/bootstrap'
+
+var jQuery = require('jquery')
+
+require("bootstrap")
+
+import "bootstrap/dist/js/bootstrap";
+
+import "../styles/application";
+document.addEventListener("turbolinks:load", function () {
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+  })
+})
 
 const images = require.context('../images', true)
-
-import '../styles/application'
