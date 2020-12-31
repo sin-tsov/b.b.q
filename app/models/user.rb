d20174cca9aee0bea37babc30f89d541264a0043
Sin63 <sincov63@gmail.com>
2020-12-31 13:42:49 +0300

    Adedd event comments and deleting events

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :events
   has_many :comments
-  has_many :subscriptions
-
+  
   validates :name, presence: true, length: {maximum: 35}
 
   before_validation :set_name, on: :create
