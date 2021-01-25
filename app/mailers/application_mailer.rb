81c331a321dd7ae1d1dec2ccff151008da8d6751
Sin63 <sincov63@gmail.com>
2021-01-25 20:41:55 +0300

    Mail setup

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,8 @@
 # Базовый класс для всех мэйлеров приложения
 class ApplicationMailer < ActionMailer::Base
   # обратный адрес всех писем по умолчанию
-  default from: ENV["MAILJET_SENDER"]
-
+  # default from: ENV["MAILJET_SENDER"]
+  default from: "rubysergey@gmail.com"
   # Задаем макет для всех писем
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
+  config.active_job.queue_adapter = :async
+  config.active_job.queue_name_prefix = "bbq_#{Rails.env}"
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -16,10 +20,10 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
+
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+        'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -58,19 +62,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.perform_deliveries = true
 
-  # config.active_job.queue_adapter = :inline
-  # config.active_job.queue_name_prefix = "b.b.q_#{Rails.env}"
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
-  config.action_mailer.smtp_settings = {
-    address:             'smtp.gmail.com',
-    port:                '587',
-    user_name:           ENV['GM_USERNAME'],
-    password:            ENV['GM_PASS'],
-    authentication:      'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.asset_host = 'localhost:3000'
+
+   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  config.active_job.queue_adapter = :inline
+  config.active_job.queue_name_prefix = "bbq_#{Rails.env}"
+
+  config.hosts << "lvh.me"
 end
